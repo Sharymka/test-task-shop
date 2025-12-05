623dfe9d12e7dfa95134acc5cbc493aa0ce6288b
svetalana <sveta.sharymova@gmail.com>
2025-12-05 10:14:23 +0400

    feature: add courses data array

--- js/data.js
+++ js/data.js
@@ -1,3 +1,147 @@
-
-const courses = [];
-
+const courses = [
+  // Marketing (4 курса)
+  {
+    id: 1,
+    title: "The Ultimate Google Ads Training Course",
+    price: 100,
+    instructor: "Jerome Bell",
+    category: "marketing",
+    image: "images/jerome-bell.png"
+  },
+  {
+    id: 4,
+    title: "Brand Management & PR Communications",
+    price: 530,
+    instructor: "Kristin Watson",
+    category: "marketing",
+    image: "images/kristin-watson.png"
+  },
+  {
+    id: 10,
+    title: "Social Media Marketing Strategy",
+    price: 350,
+    instructor: "Jerome Bell",
+    category: "marketing",
+    image: "images/jerome-bell.png"
+  },
+  {
+    id: 11,
+    title: "Content Marketing Mastery",
+    price: 420,
+    instructor: "Kristin Watson",
+    category: "marketing",
+    image: "images/kristin-watson.png"
+  },
+  
+  // Management (3 курса)
+  {
+    id: 2,
+    title: "Product Management Fundamentals",
+    price: 480,
+    instructor: "Marvin McKinney",
+    category: "management",
+    image: "images/marvin-mckinney.png"
+  },
+  {
+    id: 6,
+    title: "Business Development Management",
+    price: 400,
+    instructor: "Dianne Russell",
+    category: "management",
+    image: "images/dianne-russell.png"
+  },
+  {
+    id: 12,
+    title: "Strategic Leadership & Team Building",
+    price: 550,
+    instructor: "Marvin McKinney",
+    category: "management",
+    image: "images/marvin-mckinney.png"
+  },
+  
+  // HR & Recruiting (5 курсов)
+  {
+    id: 3,
+    title: "HR Management and Analytics",
+    price: 200,
+    instructor: "Leslie Alexander Li",
+    category: "hr",
+    image: "images/leslie-alexander-li.png"
+  },
+  {
+    id: 8,
+    title: "Human Resources - Selection and Recruitment",
+    price: 150,
+    instructor: "Kathryn Murphy",
+    category: "hr",
+    image: "images/kathryn-murphy.png"
+  },
+  {
+    id: 13,
+    title: "Employee Engagement & Retention",
+    price: 280,
+    instructor: "Leslie Alexander Li",
+    category: "hr",
+    image: "images/leslie-alexander-li.png"
+  },
+  {
+    id: 14,
+    title: "Talent Acquisition Strategies",
+    price: 320,
+    instructor: "Kathryn Murphy",
+    category: "hr",
+    image: "images/kathryn-murphy.png"
+  },
+  {
+    id: 15,
+    title: "Performance Management Systems",
+    price: 250,
+    instructor: "Leslie Alexander Li",
+    category: "hr",
+    image: "images/leslie-alexander-li.png"
+  },
+  
+  // Design (2 курса)
+  {
+    id: 5,
+    title: "Graphic Design Basic",
+    price: 500,
+    instructor: "Guy Hawkins",
+    category: "design",
+    image: "images/guy-hawkins.png"
+  },
+  {
+    id: 9,
+    title: "User Experience. Human-centered Design",
+    price: 240,
+    instructor: "Cody Fisher",
+    category: "design",
+    image: "images/cody-fisher.png"
+  },
+  
+  // Development (3 курса)
+  {
+    id: 7,
+    title: "Highload Software Architecture",
+    price: 600,
+    instructor: "Brooklyn Simmons",
+    category: "development",
+    image: "images/brooklyn-simmons.png"
+  },
+  {
+    id: 16,
+    title: "Full Stack Web Development",
+    price: 450,
+    instructor: "Brooklyn Simmons",
+    category: "development",
+    image: "images/brooklyn-simmons.png"
+  },
+  {
+    id: 17,
+    title: "Modern JavaScript Frameworks",
+    price: 380,
+    instructor: "Brooklyn Simmons",
+    category: "development",
+    image: "images/brooklyn-simmons.png"
+  }
+];
